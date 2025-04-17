59360c25ae0782a31804d23358b7d0bf77d54ad7
Nick <doroshenkonk@gmail.com>
2025-04-17 14:31:02 +0300

    Update interval_timeout_detector.js

--- interval_timeout_detector.js
+++ interval_timeout_detector.js
@@ -1,28 +1,45 @@
+// ==UserScript==
+// @name         Timer Tracker
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Tracks all setTimeout and setInterval calls on the page
+// @author       https://github.com/doroshnk
+// @match        *://*/*
+// @run-at       document-start
+// ==/UserScript==
+
 (function() {
-  const originalSetTimeout = window.setTimeout;
-  const originalSetInterval = window.setInterval;
+    'use strict';
 
-  const timeouts = [];
-  const intervals = [];
+    // Store original functions to preserve native behavior
+    const originalSetTimeout = window.setTimeout;
+    const originalSetInterval = window.setInterval;
 
-  window.setTimeout = function(callback, delay, ...args) {
-    const id = originalSetTimeout(callback, delay, ...args);
-    timeouts.push({ id, delay, callback });
-    console.log('[setTimeout]', id, 'Delay:', delay, 'Callback:', callback);
-    return id;
-  };
+    // Arrays to keep track of all timers
+    const timeouts = [];
+    const intervals = [];
 
-  window.setInterval = function(callback, delay, ...args) {
-    const id = originalSetInterval(callback, delay, ...args);
-    intervals.push({ id, delay, callback });
-    console.log('[setInterval]', id, 'Delay:', delay, 'Callback:', callback);
-    return id;
-  };
+    // Override setTimeout to log and store each timer
+    window.setTimeout = function(callback, delay, ...args) {
+        const id = originalSetTimeout(callback, delay, ...args);
+        timeouts.push({ id, delay, callback });
+        console.log('[setTimeout]', 'ID:', id, '| Delay:', delay, '| Callback:', callback);
+        return id;
+    };
 
-  window._myTimers = {
-    getTimeouts: () => timeouts,
-    getIntervals: () => intervals,
-  };
+    // Override setInterval to log and store each interval
+    window.setInterval = function(callback, delay, ...args) {
+        const id = originalSetInterval(callback, delay, ...args);
+        intervals.push({ id, delay, callback });
+        console.log('[setInterval]', 'ID:', id, '| Delay:', delay, '| Callback:', callback);
+        return id;
+    };
 
-  console.log('Timer tracking enabled. Use _myTimers.getTimeouts() or _myTimers.getIntervals() to inspect.');
+    // Expose utility functions globally for easy access
+    window._myTimers = {
+        getTimeouts: () => timeouts,
+        getIntervals: () => intervals,
+    };
+
+    console.log('Use _myTimers.getTimeouts() or _myTimers.getIntervals() in the console to inspect active timers.');
 })();
